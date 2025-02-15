1fad48ee8721ac82cf863d439c42e280916cff02
Sohila Mohamed <sohilamnasr@gmail.com>
2025-02-15 05:19:05 +0200

    Add files via upload

---
+++ Calculator_project.c
@@ -0,0 +1,17 @@
+/*
+ * Calculator_project.c
+ *
+ * Created: 4/9/2024 1:18:48 AM
+ *  Author: sohila mohamed
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
